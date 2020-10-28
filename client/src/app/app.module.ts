2b40b51026ff9ecee581ca16186d1247c89b7523
jelel-fliss <jelel.fliss@etudiant-fst.utm.tn>
2020-10-28 19:39:53 +0100

    Importing RouterModule and PagesModule to AppModule
    RouterModule.forRoot(routes) => Importing the router module to access pages via the routes passed as parameter
    PagesModule has to be imported so the AppModule, which is the parent Module, can access its components ( pages )

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routes } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { PagesRoutes } from './pages/pages-routing.module';
+import { PagesModule } from './pages/pages.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    PagesModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
