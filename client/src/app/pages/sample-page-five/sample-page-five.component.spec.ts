52400b5a8eb718c9626f370b6c9082fb29112e47
safa-max <safa.aloui2018@gmail.com>
2020-11-02 21:57:47 +0100

    c'est la 5 éme page qui correspond au derniér bouton du sidebar

---
+++ client/src/app/pages/sample-page-five/sample-page-five.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SamplePageFiveComponent } from './sample-page-five.component';
+
+describe('SamplePageFiveComponent', () => {
+  let component: SamplePageFiveComponent;
+  let fixture: ComponentFixture<SamplePageFiveComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SamplePageFiveComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SamplePageFiveComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
