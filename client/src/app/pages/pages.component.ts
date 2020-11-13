65404b9868e08b7df4b020f0db8c3359198d38ec
safa-max <safa.aloui2018@gmail.com>
2020-11-13 21:00:58 +0100

    j'ai rien ajouté içi nrmlmnt

--- client/src/app/pages/pages.component.ts
+++ client/src/app/pages/pages.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-pages',
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class PagesComponent {
   title = 'E-learning platform';
+  
+  
+
+
+	constructor() {}
+	
+  
+  ngOnInit() {};
+
+  processReq(message: any){
+  alert(message);
+  }
 }
+
