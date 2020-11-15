f2a8a9e46dc65d0e2cce31b0415ae003d0e2ac43
Eyabouka <eyaboukadhaba@gmail.com>
2020-11-15 23:31:32 +0100

    deleting sample pages 1,3,4,5

--- client/src/app/pages/pages.module.ts
+++ client/src/app/pages/pages.module.ts
@@ -9,11 +9,9 @@ import { PagesComponent } from './pages.component';
 import { HomeComponent } from './home/home.component';
 import { RouterModule } from '@angular/router';
 import { PagesRoutes } from './pages-routing.module';
-import { SamplePageOneComponent } from './sample-page-one/sample-page-one.component';
+
 import { SamplePageTwoComponent } from './sample-page-two/sample-page-two.component';
-import { SamplePageThreeComponent } from './sample-page-three/sample-page-three.component';
-import { SamplePageFourComponent } from './sample-page-four/sample-page-four.component';
-import { SamplePageFiveComponent } from './sample-page-five/sample-page-five.component';
+
 
 @NgModule({
   declarations: [
@@ -21,11 +19,7 @@ import { SamplePageFiveComponent } from './sample-page-five/sample-page-five.com
     SidebarComponent,
     PagesComponent,
     HomeComponent,
-    SamplePageOneComponent,
     SamplePageTwoComponent,
-    SamplePageThreeComponent,
-    SamplePageFourComponent,
-    SamplePageFiveComponent
   ],
   exports: [
     SidebarComponent,
