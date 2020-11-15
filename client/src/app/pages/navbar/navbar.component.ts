15bca342061bb777f0c85a67e74a60969a4cef3a
safa-max <safa.aloui2018@gmail.com>
2020-11-15 20:53:11 +0100

    envoie de la classe

--- client/src/app/pages/navbar/navbar.component.ts
+++ client/src/app/pages/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit , EventEmitter , Input, Output} from '@angular/core';
+import { SamplePageThreeComponent } from '../sample-page-three/sample-page-three.component';
 
 @Component({
   selector: 'app-navbar',
@@ -6,15 +7,25 @@ import { Component, OnInit , EventEmitter , Input, Output} from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-	@Output() SendRequestToData = new EventEmitter();
+  @Output() SendRequestToData = new EventEmitter();
+  theme :string;
   constructor() { }
 
   ngOnInit(): void {
+    this.theme = "dark-theme";
   }
   sendEvent (){
-  this.SendRequestToData.emit(
-  ` Hello`
-  );
+  if (this.theme == "dark-theme") {
+    this.theme = "white-theme";
+    this.SendRequestToData.emit(
+      this.theme
+      );
+  } else {
+    this.theme = "dark-theme";
+    this.SendRequestToData.emit(
+      this.theme
+      );
+  }
   }
 
 }
